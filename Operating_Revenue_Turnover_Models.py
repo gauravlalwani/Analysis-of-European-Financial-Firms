5b6fefd1364e7badec47e43783807bb43c9f262a
gauravlalwani <32558409+gauravlalwani@users.noreply.github.com>
2018-05-06 16:22:35 -0500

    Code for regression of Operating Revenue Turnover

--- Operating_Revenue_Turnover_Models.py
+++ Operating_Revenue_Turnover_Models.py
@@ -80,7 +80,6 @@ y = X1final.iloc[:, -1].values
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 0)
 
 
-
 # Fitting Decision Tree  to the dataset
 
 deci_score=[]
